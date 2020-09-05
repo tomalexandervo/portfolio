1e110aef85931a31abee87eaf5275cf1fa52da88
Tom Vo <lisalisatvo@gmail.com>
2020-09-05 00:34:27 -0700

    edit about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,7 +29,7 @@ class About extends React.Component {
         I'm Tom, a recent graduate from the University of Washington majoring in Geography with a minor in Informatics.
         I am passionate about creating things that look either pretty, simple, and functional. I believe things should look visually appealing while also holding on to the concept of simplicity.
         </p>
-        <p> I am currently seeking work! I'm good at front end developing, and if there's anything I don't know, I will learn.</p>
+        <p> I am currently seeking work! I'm focused in full-stack development, and if there's anything I don't know, I will learn.</p>
         <p> Aside from coding I care about things such as diversity, inclusion, education, music, geography, community, tea, and cats.</p>
         <p>My hobbies include working out, building keyboards for others, cooking, hiking, and petting cats!🐈 </p>
         <img className="mejpg"src={DisplayImage} alt={siteTitle} />
